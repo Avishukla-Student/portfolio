56a7d971e73453fc2cb55608de1dfb3208663b0c
Avi shukla <avishukla8303@gmail.com>
2024-08-06 16:49:30 +0530

    adding css

---
+++ main.js
@@ -0,0 +1,9 @@
+var options = {
+    Strings:['frontend developer','web developer',"Collage Student"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+};
+
+var typed= new typed(".text", options);
